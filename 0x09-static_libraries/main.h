778bffc1b12831bfae8322953d845bb731610594
2inlove <psm2lv@gmail.com>
2023-09-04 06:34:28 -0700

    More

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,16 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
-		
-int _putchar(char c);		
+
+int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
 int _abs(int n);
 int _isupper(int c);
 int _isdigit(int c);
 int _strlen(char *s);
-void *_puts(char *str);
+void _puts(char *str);
 char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
+int *_atoi(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
